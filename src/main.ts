25feabe000dae8a1450474e18ac944214a7d0157
Sofilus <sofia00olsson@gmail.com>
2023-02-23 15:04:47 +0100

    Made menu open and close

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import './style.scss'
+
+const menuBtn = document.querySelector('#menu-button') as HTMLButtonElement;
+menuBtn.addEventListener('click', openMenu);
+
+let menuOpen = false
+
+function openMenu() {
+    const openMenu = document.querySelector('#open-menu')
+    if (openMenu?.classList.contains('display-none')) {
+        openMenu?.classList.remove('display-none')
+    } else if (!openMenu?.classList.contains('display-none')) {
+        openMenu?.classList.add('display-none')
+    }
+
+
+}
