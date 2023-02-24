30aaaca6bb339c18d450c8d0d17b4edc4076d8ca
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-02-24 09:31:00 +0100

    fixed swiper images to work

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import './scss/style.scss';
 import { createFormHtml } from './ts/signUp';
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-const swiperElement = document.querySelector('.swiper') as HTMLElement;
-const swiper = new Swiper(swiperElement, {
-  modules: [Navigation, Pagination],
-});
 
 const menuBtn = document.querySelector('#menu-button') as HTMLButtonElement;
 menuBtn.addEventListener('click', openMenu);
